d260d882210fab3a0eb466494eb7428f15d05fcd
Dien Tran <dien.kim.tran@utdallas.edu>
2021-09-29 17:21:02 -0500

    Added findings about ingesting data into Graphite

---
+++ graphite/findings.md
@@ -0,0 +1,29 @@
+# Workflow for setting up Graphite and ingesting data through CSV
+
+Here are my current findings on how to set up Graphite and ingesting CSV data.
+
+First, make sure you are in a Linux environment with Docker and Node.js installed. Also, the Node.js package `zx` is also needed to run the script. Make sure it is installed globally, i.e. you installed it with `npm i -g zx`.
+
+Clone [this repo](https://github.com/kamon-io/docker-grafana-graphite) on GitHub:
+
+```console
+docker run -d \
+ --name graphite \
+ --restart=always \
+ -p 80:80 \
+ -p 2003-2004:2003-2004 \
+ -p 2023-2024:2023-2024 \
+ -p 8125:8125/udp \
+ -p 8126:8126 \
+ graphiteapp/graphite-statsd
+```
+
+This will spin up Graphite. Now run the following to ingest the `test.csv` file into Graphite, which was originally `475a5fe3003e0023_2020-10-26.csv` in the given dataset zip:
+
+```console
+zx ./ingest.mjs --csv ./test.csv
+```
+
+Since this CSV is quite small, ingestion should not take more than a few minutes on a decently fast computer.
+
+Next, open `localhost` (port 80) on a browser, and set the time frame to include 10/26/2020-10/27/2020, preferably small enough so we can actually see the data points in the visualization.
---
+++ graphite/ingest.mjs
@@ -0,0 +1,49 @@
+#!/usr/bin/env zx
+
+/**
+ * CSV ingestion script. Requires Node.js >= 14.13.1 and
+ * 'zx' (a Node.js package) installed to run.
+ * 
+ * Provide a CSV filename to the --csv argument. Make sure
+ * Graphite dataport is serving on port 2003 before running.
+ * 
+ * This script will parse the CSV file and put read data under
+ * `data.*`.
+ */
+import pkg from 'csv';
+const { parse } = pkg;
+
+// $.verbose = false;
+
+// Check if CSV filename given as arg
+if (!argv.csv) {
+  console.log(`Usage: zx ${__filename} --csv [CSV_FILENAME]`);
+  process.exit(0);
+}
+
+// Check if the container is running
+try {
+  await $`ss -tulpn | grep ":2003"`;
+} catch (p) {
+  console.error("No process found running on port 2003; have you started the container?");
+  process.exit(p.exitCode);
+}
+
+// Read the CSV file
+const parser = parse({
+  columns: true,
+  from: 2
+});
+const readStream = fs.createReadStream(argv.csv);
+readStream.pipe(parser);
+debugger;
+parser.on('readable', async () => {
+  let record;
+  while (record = parser.read()) {
+    for (let [field, value] of Object.entries(record)) {
+      if (['dateTime', 'id'].includes(field)) continue; // skip dateTime and id fields
+      await nothrow($`echo "data.${field} ${value} ${Math.floor(new Date(record.dateTime).getTime() / 1000)}" | nc -q0 localhost 2003`);
+    }
+  }
+})
+parser.on('end', () => process.exit(0));
