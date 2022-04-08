228601a5137495d1098f3a362f444fd452a45cb6
Eric Zhang <36653830+ez314@users.noreply.github.com>
2022-04-08 16:16:33 -0500

    Add Summary CSV processing and instructions
    Add Summary reading functionality to csvtoinflux script. As a part of
    this change, the macAddress tag has been changed to nodeId, to be in
    line with the column name in Summary files.
    
    Also updated README with quickstart instructions for running csvtoinflux and
    InfluxDB setup.
    
    .png removed from .gitignore to allow images in documentation hosted in
    GH.

--- influxdb/README.md
+++ influxdb/README.md
@@ -4,7 +4,37 @@
 - [influx wrote](https://docs.influxdata.com/influxdb/v2.1/reference/cli/influx/write/)
 - [Python client](https://github.com/influxdata/influxdb-client-python)
 
-## Running the Code
+## Quickstart
+### Start InfluxDB
+In `sharedairdfw/`, run `docker-compose up` to start InfluxDB, Grafana, and Node-RED.  
+```shell
+cd sharedairdfw
+docker-compose up
+```
+
+### Initialize InfluxDB and Influx CLI
+Log into InfluxDB at http://localhost:8086. Set the admin username/password, and remember it for later.  
+- For the org name, put `sharedairdfw`. 
+- For the default bucket, put `main`.
+
+Navigate to `Data` > `API Tokens`, click on the entry (there should only be one), and take note of the token at the 
+top of the popup.  
+
+In command line, configure `influx` with the following:
+```shell
+influx config create --config-name default \
+  --host-url http://localhost:8086 \
+  --org sharedairdfw \
+  --token <your-auth-token-from-prev-step> \
+  --active
+```
+
+[based on documentation](https://docs.influxdata.com/influxdb/cloud/reference/cli/influx/#provide-required-authentication-credentials)
+
+> TODO: This step requires you to have the `influx` tool installed locally, because the ingestion script currently 
+> does not run in the container. Should be moved inside the container.
+
+### Run the Ingestion Script
 Run the script, with the mqttSubscribers data directory as an argument.  
 ```shell
 cd influxdb
@@ -14,17 +44,26 @@ cd influxdb
 You should see output that looks similar to this:  
 ```
 Loading GPGGALR (../mqttSubscribers_dump/rawMqtt/471a55800038004e/2022/03/25/MINTS_471a55800038004e_GPGGALR_2022_03_25.csv) into InfluxDB
+Detected single-sensor CSV
 Loading IPS7100 (../mqttSubscribers_dump/rawMqtt/471a55800038004e/2022/03/25/MINTS_471a55800038004e_IPS7100_2022_03_25.csv) into InfluxDB
+Detected single-sensor CSV
 Loading BME280 (../mqttSubscribers_dump/rawMqtt/471a55800038004e/2022/03/25/MINTS_471a55800038004e_BME280_2022_03_25.csv) into InfluxDB
+Detected single-sensor CSV
 Loading INA219Duo (../mqttSubscribers_dump/rawMqtt/471a55800038004e/2022/03/25/MINTS_471a55800038004e_INA219Duo_2022_03_25.csv) into InfluxDB
+Detected single-sensor CSV
 Loading PM (../mqttSubscribers_dump/rawMqtt/471a55800038004e/2022/03/25/MINTS_471a55800038004e_PM_2022_03_25.csv) into InfluxDB
+Detected single-sensor CSV
 Loading BME280 (../mqttSubscribers_dump/rawMqtt/478b5fe30040004b/2022/03/25/MINTS_478b5fe30040004b_BME280_2022_03_25.csv) into InfluxDB
-Loading GPGGALR (../mqttSubscribers_dump/rawMqtt/478b5fe30040004b/2022/03/25/MINTS_478b5fe30040004b_GPGGALR_2022_03_25.csv) into InfluxDB
-Loading IPS7100 (../mqttSubscribers_dump/rawMqtt/478b5fe30040004b/2022/03/25/MINTS_478b5fe30040004b_IPS7100_2022_03_25.csv) into InfluxDB
-Loading PM (../mqttSubscribers_dump/rawMqtt/478b5fe30040004b/2022/03/25/MINTS_478b5fe30040004b_PM_2022_03_25.csv) into InfluxDB
-Loading INA219Duo (../mqttSubscribers_dump/rawMqtt/478b5fe30040004b/2022/03/25/MINTS_478b5fe30040004b_INA219Duo_2022_03_25.csv) into InfluxDB
-Loading PM (../mqttSubscribers_dump/rawMqtt/47db5580001e0039/2022/03/25/MINTS_47db5580001e0039_PM_2022_03_25.csv) into InfluxDB
-Loading BME280 (../mqttSubscribers_dump/rawMqtt/47db5580001e0039/2022/03/25/MINTS_47db5580001e0039_BME280_2022_03_25.csv) into InfluxDB
-Loading IPS7100 (../mqttSubscribers_dump/rawMqtt/47db5580001e0039/2022/03/25/MINTS_47db5580001e0039_IPS7100_2022_03_25.csv) into InfluxDB
-Loading GPGGALR (../mqttSubscribers_dump/rawMqtt/47db5580001e0039/2022/03/25/MINTS_47db5580001e0039_GPGGALR_2022_03_25.csv) into InfluxDB
+Detected single-sensor CSV
+...
 ```
+
+### Display Data
+In the InfluxDB dashboard, navigate to `Explore`, and select the `sharedairdfw` bucket. Configure the filters to 
+whatever you want, make sure the time range (top right of bottom panel) surrounds the data you want to graph, and 
+hit `Submit`. 
+
+You may want to use a custom, 1-minute mean aggregate function to view the trends in more detail.  
+
+Example:  
+![Example showing custom aggregate function](doc-img/Screen%20Shot%202022-04-08%20at%204.12.52%20PM.png)
--- influxdb/csvtoinflux.sh
+++ influxdb/csvtoinflux.sh
@@ -14,59 +14,98 @@ fi
 function loadCsvToInflux() {
   local filepath=$1
   local filename
-  local mac
-  local sensor
+  local part1
+  local part2
   local header
   local datatype=''
 
-  # Do not process if cannot parse sensor name & mac from title
+  # Do not process if cannot parse part2 (sensor name or "Summary") & part1 (mac address or "LoraNodes") from file name
   filename=$(basename "$filepath")
-  if [[ ! "$filename" =~ MINTS_[0-9a-z]*_[0-9A-Za-z_]*_[0-9]{4}_[0-9]{2}_[0-9]{2}.csv ]]; then
+  if [[ ! "$filename" =~ MINTS_[0-9A-Za-z]*_[0-9A-Za-z_]*_[0-9]{4}_[0-9]{2}_[0-9]{2}.csv ]]; then
     echo "Skipping because $filename did not match the required format."
     exit
   fi
 
-  # Parse sensor name & mac from title
-  mac=$(echo "$filename" | perl -pe 's/MINTS_(.*)_(.*)_\d{4}_\d{2}_\d{2}.csv/\1/')
-  sensor=$(echo "$filename" | perl -pe 's/MINTS_(.*)_(.*)_\d{4}_\d{2}_\d{2}.csv/\2/')
+  # Parse part2 name & part1 from title
+  part1=$(echo "$filename" | perl -pe 's/MINTS_(.*)_(.*)_\d{4}_\d{2}_\d{2}.csv/\1/')
+  part2=$(echo "$filename" | perl -pe 's/MINTS_(.*)_(.*)_\d{4}_\d{2}_\d{2}.csv/\2/')
 
-  echo "Loading $sensor ($filepath) into InfluxDB"
+  echo "Loading $part2 ($filepath) into InfluxDB"
 
-  # Build datatype string from column names
-  header=$(head -1 "$filepath")
-  IFS=',' read -ra headerArr <<< "$header"
+  # Determine if this is a summary or individual sensor CSV
+  if [[ "$part2" == 'Summary' ]]; then
+    echo "Detected summarized CSV"
 
-  for col in "${headerArr[@]}"; do
-    if [[ "$col" == 'dateTime' ]]; then
-      datatype="${datatype},dateTime"
-    elif [[ "$col" =~ timestamp|dateStamp ]]; then
-      datatype="${datatype},ignored"
-    elif [[ "$col" =~ status|Direction|Units|base64Data|base16Data|devAddr|deviceAddDecoded|codeRate|ID ]]; then
-      datatype="${datatype},string"
-    else
-      datatype="${datatype},field"
-    fi
-  done
 
-  # Remove first comma
-  datatype=${datatype:1}
+    # Build datatype string from column names
+    # https://docs.influxdata.com/influxdb/v2.1/reference/syntax/annotated-csv/extended/
+    header=$(head -1 "$filepath")
+    IFS=',' read -ra headerArr <<< "$header"
 
-  # Perform some edits on the CSV, and load into influx using influx write.
+    for col in "${headerArr[@]}"; do
+      if [[ "$col" == 'sensorID' ]]; then
+        datatype="${datatype},measurement"
+      elif [[ "$col" == 'nodeId' ]]; then
+        datatype="${datatype},tag"
+      elif [[ "$col" == 'dateTime' ]]; then
+        datatype="${datatype},dateTime"
+      elif [[ "$col" =~ timestamp|dateStamp ]]; then
+        datatype="${datatype},ignored"
+      elif [[ "$col" =~ status|Direction|Units|base64Data|base16Data|devAddr|deviceAddDecoded|codeRate|ID ]]; then
+        datatype="${datatype},string"
+      else
+        datatype="${datatype},field"
+      fi
+    done
 
-  # The replacements are for:
-  #  1. Convert timestamp to RFC3339 format
-  #  2. Remove prefix spaces (occurs in some files)
-  #  3. Remove NANs
-  #  4. Add trailing comma to lines with trailing comma, due to a bug in the CSV parser. This bug reads the first value
-  #     of the next line as the last value of the current line, if there is no value specified in the CSV (i.e. it's
-  #     empty)
-  perl -pe 's/^([0-9-]+) ([0-9:.]+)/\1T\2Z/ and s/,\s+/,/g and s/NAN//g and s/,$/,,/' < "$filepath" | \
-  influx write \
-    --bucket sharedairdfw \
-    --format csv \
-    --header "#constant measurement,$sensor" \
-    --header "#constant tag,macAddress,$mac" \
-    --header "#datatype $datatype"
+    # Remove first comma
+    datatype=${datatype:1}
+
+    # Perform some edits on the CSV, and load into influx using influx write.
+    perl -pe 's/^([0-9-]+) ([0-9:.]+)/\1T\2Z/ and s/,\s+/,/g and s/NAN//g and s/,$/,,/' < "$filepath" | \
+    influx write \
+      --bucket sharedairdfw \
+      --format csv \
+      --header "#datatype $datatype"
+  else
+    echo "Detected single-sensor CSV"
+    # Build datatype string from column names
+    # https://docs.influxdata.com/influxdb/v2.1/reference/syntax/annotated-csv/extended/
+    header=$(head -1 "$filepath")
+    IFS=',' read -ra headerArr <<< "$header"
+
+    for col in "${headerArr[@]}"; do
+      if [[ "$col" == 'dateTime' ]]; then
+        datatype="${datatype},dateTime"
+      elif [[ "$col" =~ timestamp|dateStamp ]]; then
+        datatype="${datatype},ignored"
+      elif [[ "$col" =~ status|Direction|Units|base64Data|base16Data|devAddr|deviceAddDecoded|codeRate|ID ]]; then
+        datatype="${datatype},string"
+      else
+        datatype="${datatype},field"
+      fi
+    done
+
+    # Remove first comma
+    datatype=${datatype:1}
+
+    # Perform some edits on the CSV, and load into influx using influx write.
+
+    # The replacements are for:
+    #  1. Convert timestamp to RFC3339 format
+    #  2. Remove prefix spaces (occurs in some files)
+    #  3. Remove NANs
+    #  4. Add trailing comma to lines with trailing comma, due to a bug in the CSV parser. This bug reads the first value
+    #     of the next line as the last value of the current line, if there is no value specified in the CSV (i.e. it's
+    #     empty)
+    perl -pe 's/^([0-9-]+) ([0-9:.]+)/\1T\2Z/ and s/,\s+/,/g and s/NAN//g and s/,$/,,/' < "$filepath" | \
+    influx write \
+      --bucket sharedairdfw \
+      --format csv \
+      --header "#constant measurement,$part2" \
+      --header "#constant tag,nodeId,$part1" \
+      --header "#datatype $datatype"
+  fi
 }
 
 export -f loadCsvToInflux
