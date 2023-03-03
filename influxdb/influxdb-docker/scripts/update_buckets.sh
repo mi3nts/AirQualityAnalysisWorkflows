f89eb0072d71120b2fcd952e6a8636b1fe1fe3af
John Waczak <john.louis.waczak@gmail.com>
2023-03-03 11:30:03 -0600

    okay... I think this should do the initialization now

--- influxdb/influxdb-docker/scripts/update_buckets.sh
+++ influxdb/influxdb-docker/scripts/update_buckets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+# set -e
 
 # run command to add bucket
-influx bucket create -n open-aq 
+influx bucket create -n open-aq -o $DOCKER_INFLUXDB_INIT_ORG -t $DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
