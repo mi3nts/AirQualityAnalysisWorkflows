76c4a482d2771633eac7c838339fe7d4b9ece5ab
neshuynh <neshuynh@gmail.com>
2023-03-31 13:39:06 -0500

    Ofelia setup to work with nodered schedule
    - Adjusted the uvwind script to access the raw_data file in it's directory.
    - Added Ofelia to the docker-compose that restarts the miniconda container every 6 hours.
    - Changed the miniconda dockerfile to run the uvwind script every time it is started.
    - Removed miniconda test files

--- wms/conda_scripts/uvwind.sh
+++ wms/conda_scripts/uvwind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Copying files..."
-grib_copy -w count=585/586 /data/test.grb2 /data/output_u_v_combined.grb2
+grib_copy -w count=585/586 /data/*/*/*/raw_data.grib2 /data/output_u_v_combined.grb2
 echo "All done!"
