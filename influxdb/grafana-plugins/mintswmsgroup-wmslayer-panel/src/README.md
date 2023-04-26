b03d1dcf3b124f2cefc615a3dd1899478ea4efdf
Fernando Colman <fernando.colman01@outlook.com>
2023-04-26 18:45:08 -0500

    Remove Old Signed Plugin and Made a New Copy

--- influxdb/grafana-plugins/mintswmsgroup-wmslayer-panel/src/README.md
+++
@@ -1,5 +0,0 @@
-<!-- This README file is going to be the one displayed on the Grafana.com website for your plugin -->
-
-# Wms Layer
-
-Plugin to visualize wms data on grafana
--- influxdb/grafana-plugins/mintswmsgroup-wmslayer-panel/src/types.ts
+++
@@ -1,9 +0,0 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
-type CircleColor = 'red' | 'green' | 'blue';
-
-export interface SimpleOptions {
-  text: string;
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
-  color: CircleColor;
-}
