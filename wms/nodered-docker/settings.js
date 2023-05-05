af0ae0c739a2d55f2361c19a96efc036993a9d0b
neshuynh <neshuynh@gmail.com>
2023-05-05 13:44:28 -0500

    Finished changing NodeRed's port to 1890
    Missed a line in the settings.js file

--- wms/nodered-docker/settings.js
+++ wms/nodered-docker/settings.js
@@ -142,7 +142,7 @@ module.exports = {
  ******************************************************************************/
 
     /** the tcp port that the Node-RED web server is listening on */
-    uiPort: process.env.PORT || 1880,
+    uiPort: process.env.PORT || 1890,
 
     /** By default, the Node-RED UI accepts connections on all IPv4 interfaces.
      * To listen on all IPv6 addresses, set uiHost to "::",
